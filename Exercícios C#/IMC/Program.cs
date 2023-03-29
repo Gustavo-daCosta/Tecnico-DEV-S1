ea0b54417c8f390b50819f1167504c31a701add3
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-03-29 14:43:37 -0300

    Adicionado Introdução ao C# e Exercício de IMC

---
+++ Exercícios C#/IMC/Program.cs
@@ -0,0 +1,11 @@
+﻿// Crie um programa para calcular o IMC de uma pessoa
+
+// Entradas de dados
+string nome = "Gustavo";
+float peso = 78.5F;
+float altura = 1.72F;
+
+// Processamento dos dados
+float imc = peso / (altura * altura);
+
+Console.WriteLine($"O IMC do {nome} é de {Math.Round(imc, 2)}");
