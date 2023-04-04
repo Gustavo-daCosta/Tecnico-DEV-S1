46e18bcb97f6c6492dbedfb6f1fb24e9120e5b18
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-04-04 17:16:49 -0300

    Início da aula de estrutura de repetição

---
+++ Estruturas de Repetição C#/Do While Loop/Program.cs
@@ -0,0 +1,15 @@
+﻿int idadeCerta = 0;
+
+do {
+    Console.Write($"Qual a idade do Adulto Ney? ");
+    int idade =int.Parse(Console.ReadLine()!);
+
+    if (idade == 32) {
+        Console.WriteLine($"Acertou !!S");
+        idadeCerta = 0;
+    }
+    else {
+        Console.WriteLine($"Errou feio ! Tente novamente ");
+    }
+
+} while (idadeCerta == 32);
