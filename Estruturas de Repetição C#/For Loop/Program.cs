cc293ede71e150ef130dc033dee3570c2b79e2c4
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-04-05 17:20:46 -0300

    Exemplos de loops For e While adicionados

---
+++ Estruturas de Repetição C#/For Loop/Program.cs
@@ -0,0 +1,9 @@
+﻿// for (int x = 0; x <= 100; x++) {
+//     Console.WriteLine(x);
+// }
+
+// Criar um programa para imprimir valores de 500 até 0, decrementar de 5 em 5+
+
+for (int i = 500; i >= 0; i -= 5) {
+    Console.WriteLine(i);
+}
---
+++ Estruturas de Repetição C#/While Loop/Program.cs
@@ -0,0 +1,13 @@
+﻿// int i = 0;
+
+// while (i < 5) {
+//     Console.WriteLine(i);
+//     i++;
+// }
+
+int idadeCerta = 0;
+
+while (idadeCerta != 32) {
+    Console.Write($"Qual é a idade do Tyler, The Creator? ");
+    idadeCerta = int.Parse(Console.ReadLine()!);
+}
