f4f5bc2a801be727c035beeb294279c7acc2329f
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-04-14 14:59:30 -0300

    Finalização dos exercícios de fixação

---
+++ Exercícios C#/Exercícios de fixação/Vetores - Cadastro de pessoas/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] nomes = new string[10];
+
+for (int i = 0; i < 10; i++) {
+    Console.Write($"Digite o nome da {i + 1}º pessoa: ");
+    nomes[i] = Console.ReadLine()!.ToLower();
+}
+
+Console.Write($"Digite o nome da pessoa que você deseja buscar: ");
+string nome = Console.ReadLine()!.ToLower();
+
+if (nomes.Contains(nome)) {
+    Console.WriteLine($"O nome FOI encontrado no vetor");
+} else {
+    Console.WriteLine($"O nome NÃO FOI encontrado no vetor");
+}
---
+++ Exercícios C#/Exercícios de fixação/Vetores - Números inversos/Program.cs
@@ -0,0 +1,10 @@
+﻿float[] valores = new float[10];
+
+for (int i = 0; i < 10; i++) {
+    Console.Write($"Digite o {i + 1}º valor: ");
+    valores[i] = float.Parse(Console.ReadLine()!);
+}
+
+foreach (float valor in valores.Reverse()) {
+    Console.Write($"{valor} ");
+}
