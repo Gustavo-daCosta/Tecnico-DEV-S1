c0adab40a6cceb092f55c8de24905ff3bd416190
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-05-19 17:11:22 -0300

    Adicionada aula de Tratamento de erros

---
+++ Tratamento de erros C#/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write($"Digite um número: ");
+string txtNumero = Console.ReadLine()!;
+
+try {
+    int n = int.Parse(txtNumero);
+
+    Console.WriteLine($"O dobro de {n} é {n * 2}");
+} catch (System.Exception error) {
+    Console.WriteLine($"Deu pau, chame o programador");
+    Console.WriteLine($"Erro: {error.Message}");
+}
